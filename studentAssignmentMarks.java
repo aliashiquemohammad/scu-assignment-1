75ed30b548c60c22d3ce5f6bc0431ad56d34c839
unique-ashique <ashique@fullstack-wp.com>
2023-09-03 05:42:16 +0800

    Class is created with importing scanner.

---
+++ studentAssignmentMarks.java
@@ -0,0 +1,37 @@
+
+/**
+ * The program allows user to input the assignment name and 30 student Marks.
+ * It shows the highest and lowest marks from the student marks.
+ * It also calculates the mean and deviation.
+ * 
+ * @author (Mohammad Ashique Ali)
+ * @version (03/09/2023)
+ */
+import java.util.Scanner;
+ 
+public class studentAssignmentMarks
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class studentAssignmentMarks
+     */
+    public studentAssignmentMarks()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
