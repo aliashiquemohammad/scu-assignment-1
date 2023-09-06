7cc60a413527a4842a165e2f725b2c8019265d8d
unique-ashique <ashique@fullstack-wp.com>
2023-09-06 23:57:26 +0800

    Input marks and input assignment with their result done

--- studentAssignmentMarks.java
+++ studentAssignmentMarks.java
@@ -5,13 +5,13 @@
  * It also calculates the mean and deviation.
  * 
  * @author (Mohammad Ashique Ali)
- * @version (03/09/2023)
+ * @version (06/09/2023)
  */
 import java.util.Scanner;
  
 public class studentAssignmentMarks
 {
-    // instance variables - replace the example below with your own
+    // instance variables
     private int[] studentAssignmentMarks;
     private String studentAssignmentName;
 
@@ -21,18 +21,67 @@ public class studentAssignmentMarks
     public studentAssignmentMarks()
     {
         // initialise instance variables with array for 30
-        studentAssignmentMarks = new int[30];
+        studentAssignmentMarks = new int[5];
     }
-
+    
     /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * Prompt for user to enter assignment name
      */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public void getAssignmentName(){
+        Scanner inputAssignmentName = new Scanner(System.in);
+        System.out.print( "Enter Assignment Name : " );
+        studentAssignmentName = inputAssignmentName.nextLine();
+        
+    }
+    
+    /**
+     * Prompt for user to enter student marks
+     * invalid input data is not accepted.
+     */
+    public void getStudentMarks(){
+        Scanner inputStudentMarks = new Scanner(System.in);
+        System.out.print(studentAssignmentMarks.length);
+        
+        int counter = 1;
+        for (int i=0; i < studentAssignmentMarks.length; i++ ){
+            // setting a flag to get error result
+            boolean inputError = false;
+            while( !inputError ){
+                System.out.print( "Enter marks for student number " + counter + " : " );
+                int studentMark = inputStudentMarks.nextInt();
+                if( studentMark > 0 && studentMark <= 30 ){
+                    counter++;
+                    studentAssignmentMarks[i] = studentMark;
+                    inputError = true;
+                }else{
+                    System.out.print( "Marks should be between 0 to 30. Please try again." );
+                }
+            }
+        }
+    }
+    
+    /**
+     * 
+     */
+    public void showAssignmentNameAndStudentMarks(){
+        System.out.println("Assignment Name : " + studentAssignmentName);
+        System.out.println("All Student Marks");
+        int counter = 1;
+        for (int i = 0; i < studentAssignmentMarks.length; i++) {
+            counter++;
+            System.out.println("Mark of student " +  counter  + " : " + studentAssignmentMarks[i]);
+        }
+    }
+    
+    /**
+     * Default method to run the whole program by having
+     * a instance of the class itself and calling all the method one by one.
+     */
+    public static void main(String[] args) {
+        studentAssignmentMarks marks = new studentAssignmentMarks();
+
+        marks.getAssignmentName();
+        marks.getStudentMarks();
+        marks.showAssignmentNameAndStudentMarks();
     }
 }
