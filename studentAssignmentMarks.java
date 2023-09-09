867648488c20300fc7a3244e65daf58af623d908
unique-ashique <ashique@fullstack-wp.com>
2023-09-10 06:29:03 +0800

    Algorith, mean, standard deviation

--- studentAssignmentMarks.java
+++ studentAssignmentMarks.java
@@ -5,7 +5,7 @@
  * It also calculates the mean and deviation.
  * 
  * @author (Mohammad Ashique Ali)
- * @version (06/09/2023)
+ * @version (10/09/2023)
  */
 import java.util.Scanner;
  
@@ -14,6 +14,7 @@ public class studentAssignmentMarks
     // instance variables
     private int[] studentAssignmentMarks;
     private String studentAssignmentName;
+    private double meanOfStudentMarks;
 
     /**
      * Constructor for objects of class studentAssignmentMarks
@@ -22,6 +23,7 @@ public class studentAssignmentMarks
     {
         // initialise instance variables with array for 30
         studentAssignmentMarks = new int[5];
+        meanOfStudentMarks = 0.0;
     }
     
     /**
@@ -29,7 +31,7 @@ public class studentAssignmentMarks
      */
     public void getAssignmentName(){
         Scanner inputAssignmentName = new Scanner(System.in);
-        System.out.print( "Enter Assignment Name : " );
+        System.out.println( "Enter Assignment Name : " );
         studentAssignmentName = inputAssignmentName.nextLine();
         
     }
@@ -40,7 +42,6 @@ public class studentAssignmentMarks
      */
     public void getStudentMarks(){
         Scanner inputStudentMarks = new Scanner(System.in);
-        System.out.print(studentAssignmentMarks.length);
         
         int counter = 1;
         for (int i=0; i < studentAssignmentMarks.length; i++ ){
@@ -49,12 +50,12 @@ public class studentAssignmentMarks
             while( !inputError ){
                 System.out.print( "Enter marks for student number " + counter + " : " );
                 int studentMark = inputStudentMarks.nextInt();
-                if( studentMark > 0 && studentMark <= 30 ){
+                if( studentMark > 0 && studentMark <= studentAssignmentMarks.length ){
                     counter++;
                     studentAssignmentMarks[i] = studentMark;
                     inputError = true;
                 }else{
-                    System.out.print( "Marks should be between 0 to 30. Please try again." );
+                    System.out.println( "Marks should be between 0 to 30. Please try again." );
                 }
             }
         }
@@ -74,6 +75,55 @@ public class studentAssignmentMarks
     }
     
     /**
+     * Calculating Mean from the student Marks
+     */
+    public void calculateMeanFromStudentMarks() {
+        int totalSumOfAssignmentMarks = 0;
+        for (int i = 0; i < studentAssignmentMarks.length; i++) {
+            totalSumOfAssignmentMarks += studentAssignmentMarks[i];
+        }
+        meanOfStudentMarks = (double) totalSumOfAssignmentMarks / studentAssignmentMarks.length;
+        System.out.println( "The mean is: " + meanOfStudentMarks );
+
+    }
+    
+    /**
+     * Calculating standard deviation from the student marks without
+     * any pre defined function
+     */
+    public void calculateStandardDeviationFromStudentMarks(){
+        double totalSumOfSquares = 0;
+        double standardDeviationOfStudentMarks;
+        
+        for (int i = 0; i < studentAssignmentMarks.length; i++) {
+            double powerBaseNumber =  studentAssignmentMarks[i] - meanOfStudentMarks;
+            int powerNumber = 2;
+            double sumOfPower = 1;
+            
+            while( powerNumber != 0 ){
+                sumOfPower *= powerBaseNumber;
+                powerNumber--;
+            }
+            
+            totalSumOfSquares += sumOfPower;
+        }
+        double numberToGetSquareRootOf =  totalSumOfSquares / studentAssignmentMarks.length;
+        
+        //temporary variable manuplate data
+        double flagVariable;  
+        double squareRootOfMarks = numberToGetSquareRootOf / 2;  
+        do{  
+            flagVariable = squareRootOfMarks;  
+            squareRootOfMarks = ( flagVariable + ( numberToGetSquareRootOf / flagVariable ) ) / 2;  
+        }
+        while( ( flagVariable - squareRootOfMarks ) != 0 );
+          
+        standardDeviationOfStudentMarks = squareRootOfMarks;
+    
+
+        System.out.println("The Standard Deviation: " + standardDeviationOfStudentMarks);
+    }
+    /**
      * Default method to run the whole program by having
      * a instance of the class itself and calling all the method one by one.
      */
@@ -83,5 +133,7 @@ public class studentAssignmentMarks
         marks.getAssignmentName();
         marks.getStudentMarks();
         marks.showAssignmentNameAndStudentMarks();
+        marks.calculateMeanFromStudentMarks();
+        marks.calculateStandardDeviationFromStudentMarks();
     }
 }
